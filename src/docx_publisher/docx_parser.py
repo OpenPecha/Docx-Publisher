b3544bad101c93c9128af662d30ecf0e2904fcf5
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-05-08 12:41:59 +0530

    added start index

--- src/docx_publisher/docx_parser.py
+++ src/docx_publisher/docx_parser.py
@@ -1,43 +1,43 @@
 import docx
 from pathlib import Path
 from time import sleep
+import uuid
 
 
 
 def split_docx(doc):
-    texts = []
-    in_language1 = True
+    splitted_files = []
     last_font_size = 0
-    language1 = []
-    language2 = []
-    for cur_para in doc.paragraphs:
+    texts = []
+    for i,cur_para in enumerate(doc.paragraphs):
         print(cur_para.text)
         print(len(cur_para.text))
         
         if cur_para.style.name == 'Heading 1':
-            if cur_para.style.font.size != last_font_size:
-                in_language1 = not in_language1
-        if in_language1:
-            language1.append(cur_para.text)
-        else:
-            language2.append(cur_para.text)
+            if cur_para.style.font.size != last_font_size and i != 0:
+                splitted_files.append('\n'.join(texts))
+                texts = []
+        texts.append(cur_para.text)
         last_font_size = cur_para.style.font.size if len(cur_para.text)!=0 else last_font_size
 
-    texts = ['\n'.join(language1),'\n'.join(language2)]
-    return texts
+    return splitted_files
 
-def create_repo(text,title):
-    repo_name = "XYZ"
-    text_path = f"{repo_name}/{title}.txt"
-    Path(repo_name).mkdir()
+def create_repo(text,start_index):
+    repo_path = f"./data/{str(uuid.uuid4())[:4]}"
+    repo_path = f"./data/{start_index}"
+
+    Path(repo_path).mkdir(exist_ok=True, parents=True)
+    text_path = f"{repo_path}/{start_index}_{str(uuid.uuid4())[:4]}.txt"
     Path(text_path).write_text(text)
-    return repo_name
+    return repo_path
 
-def create_repos(texts,title):
+def create_repos(texts):
+    start_index = 7000
     repos = []
-    for text in texts:
-        repos.append(create_repo(text,title))
-        sleep(2)
+    for i,text in enumerate(texts):
+        if i % 2 == 0 and i != 0:
+            start_index += 1
+        repos.append(create_repo(text,start_index))
     
     return repos
 
@@ -45,10 +45,10 @@ def main(docx_path):
     doc = docx.Document(docx_path)
     title = doc.core_properties.title
     texts = split_docx(doc)
-    repos = create_repos(texts,title)
+    repos = create_repos(texts)
 
 if __name__ == "__main__":
-    docx_path = "'EN_BO_WEB/article_docx/01.བོད་འགྱུར་དྲ་རྩོམ།.docx'"
+    docx_path = "EN_BO_WEB/article_docx/03.བོད་འགྱུར་དྲ་རྩོམ།.docx"
     main(docx_path)
 
 
